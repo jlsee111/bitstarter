bb3c2d9e29db50e4deb337e0adfde5665c2688e1
John Lee <jongsang76@gmail.com>
2013-08-13 04:03:37 +0000

    change applied

--- web.js
+++ web.js
@@ -1,27 +1,15 @@
-//var express = require('express');
+var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
-var fs=require('fs');
-fs.readFile(path.join(process.cwd(), 'index2.html'), function(err,data){
-	if(err) throw err;
-	console.log(data.toString());
+var app = express.createServer(express.logger());
 
-
+app.get('/', function(request, response) {
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-//var app = express.createServer(express.logger());
-
-//var content = fs.readFileSync('index.html');
-
-//app.get('/', function(request, response){
-
-//response.send(content.toString("utf-8"));
-
-//});
-
-
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//console.log("Listening on " + port);
-//});
-
-
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
